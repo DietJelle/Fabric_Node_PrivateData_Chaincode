64daf6db0330d8130a3c0008c3018f6713143f6a
jelle <jelle@192.168.1.100>
2019-05-14 22:20:53 +0200

    Node chaincode for IBM cloud

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const FishContract = require('./lib/fish-contract');
+
+module.exports.FishContract = FishContract;
+module.exports.contracts = [ FishContract ];
